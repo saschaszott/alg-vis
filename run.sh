6e61e53e5c536b5ece8d215776a615ecbb659a49
Sascha Szott <szott@gmx.de>
2019-09-09 23:05:55 +0200

    fixed invalid shebang

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-#!/bin/#!/usr/bin/env bash
+#!/usr/bin/env bash
+
+# vor der Ausführung unter Lubuntu muss die Zeile
+#   assistive_technologies=org.GNOME.Accessibility.AtkWrapper
+# in der Datei /etc/java-8-openjdk/accessibility.properties
+# auskommentiert werden (sudo)
 
 ./gradlew clean run
